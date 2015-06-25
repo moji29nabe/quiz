dccee0b795428cfc2d635a02af85cc7e1017d51b
moji29nabe <moji29nabe@localhost>
2015-06-26 00:42:52 +0900

    Modified to use sample number or 2nd argument. Fixed logic 3 digits used.

--- put_comma.js
+++ put_comma.js
@@ -1,11 +1,25 @@
-var num = 10000000000;
+var num;
+var num_str;
 var result = '';
 
-while (num > 100) {
-	var three_digits = num.toString().slice(-3);
-	// result = '' === result ? ',' + three_digits : ',' + three_digits + result;
-	result = ',' + three_digits + result;
-	num = Math.floor(num / 100);
+// check args
+if (process.argv.length < 3) { // sample value
+  num = Math.floor(Math.random() * 1000000000) + 1;
+} else {
+  num = process.argv[2];
 }
-result = num + result;
+num_str = num.toString();
+
+console.log('input: ' + num_str);
+
+while (num_str.length > 0) {
+  var three_digits = num_str.slice(-3); // get 3 digits
+  result = three_digits + result;
+
+  num_str = num_str.slice(0, -3);	// remove 3 digits
+  if (num_str.length >= 1) {
+    result = ',' + result;
+  }
+}
+
 console.log(result);
